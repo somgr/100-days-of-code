8a311c478943f887fa79176cf68eb6e7012344d5
somgr <deniska9767@gmail.com>
2022-07-21 00:31:44 +0500

    Band name generator

---
+++ 1_day Band Name Generator.py
@@ -0,0 +1,6 @@
+print('Welcome to the Band Name Generator')
+print("What's name of the city you grew up in?")
+city = input()
+print("What's your pet's name?")
+pet = input()
+print(f'Your band could be "{city} {pet}"')
